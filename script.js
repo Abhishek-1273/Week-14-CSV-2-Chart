ad401afb8f5d29ad44c23b04bb215f44b7196745
Abhishek 1273 <abhi.l127375@gmail.com>
2025-09-15 22:54:20 +0530

    Fromatting

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function setupCSVChart(fileInputId, typeInputId, canvasId, downloadBtnId) {
   const dpr = window.devicePixelRatio || 1;
   canvas.width = canvas.clientWidth * dpr;
   canvas.height = canvas.clientHeight * dpr;
-  ctx.setTransform(dpr, 0, 0, dpr, 0, 0); 
+  ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
   let chartInstance;
 
   fileInput.addEventListener('change', function () {
